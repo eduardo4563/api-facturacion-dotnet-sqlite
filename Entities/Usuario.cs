2e550e4a81ff3e0fb4f31d14bae1fcc99e29e225
Eduardo jahir Montaño Condemayta <91337378+eduardo4563@users.noreply.github.com>
2026-08-18 09:29:03 -0500

    Add default values for Username, PasswordHash, and Rol

--- Entities/Usuario.cs
+++ Entities/Usuario.cs
@@ -1,7 +1,10 @@
 public class Usuario
 {
     public int Id { get; set; }
+
     public string Username { get; set; } = "";
+
     public string PasswordHash { get; set; } = "";
+
     public string Rol { get; set; } = "Admin";
 }
